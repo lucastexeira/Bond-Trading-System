e4fe009a641775d50cf3ca2dafde873f24e4b6f0
sandrogr96 <sandrogr0996@gmail.com>
2017-08-17 19:29:09 -0300

    Class Bond
    First class

---
+++ Class/class Bond.py
@@ -0,0 +1,17 @@
+class Bond:
+
+    def __init__(self,notional maturity,coupon,interest):
+
+        self.notional = notional
+        self.maturity = maturity
+        self.coupon = coupon
+        self.interest = interest
+
+    def cashflows(self):
+        pass
+
+    def todayPriceCashflows:
+        pass
+
+    def price(self):
+        return sum(self.todayPriceCashflows()) 
