6bf36c8c6137fa6af930105d813072f944a5d2c1
lucastexeira <lucasnahueltexeira@gmail.com>
2017-08-22 20:01:51 -0300

    Aniadida logica para los metodos sin realizar
    Se agrega la logica de los metodos 'presentValues' y 'price'. Ademas se corrigieron algunos errores

--- Class/class Bond.py
+++ Class/class Bond.py
@@ -1,3 +1,5 @@
+import math
+
 class Bond:
 
     def __init__(self,notional,maturity,coupon,interest):
@@ -6,15 +8,21 @@ class Bond:
         self.maturity = maturity
         self.coupon = coupon
         self.interest = interest
+        self.frequency = 0.5
 
     def cashflows(self):
-        numCashflows = int(self.matuarity / self.frecuency)
-        l = [ self.notional * self.coupon * self.frecuency / 100.0 for i in range (numClashflows) ]
+        numCashflows = int(self.matuarity / self.frequency)
+        l = [ self.notional * self.coupon * self.frequency / 100.0 for i in range (numCashflows) ]
         l[-1] = l[-1] + self.notional
         return l
 
-    def todayPriceCashflows(self):
-        pass
+    def presentValues(self):
+        ''' today's values of the cashflows '''
+        presentValues = []
+        for i, cashflow in enumerate(self.cashflows(), 1):
+            presentValues.append(cashflow / math.pow(1 + interest, frequency * i))
+        return presentValues
 
     def price(self):
-        return sum(self.todayPriceCashflows()) 
+        ''' in $100 USD '''
+        return sum(self.presentValues()) / self.notional * 100.0 
